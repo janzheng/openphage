5f63ffb69512cfd2a5c12fd934a4897ca8fe285e
Jan Zheng <janeazy@gmail.com>
2023-11-09 00:21:27 +1100

    better styling; homepage

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+
+This is a simple backend to pull data from the Open Phage Google Sheet project: https://docs.google.com/spreadsheets/d/1fhBigiisdCc8-YWD4K8U6yvXK47p67KzA7JfAWsh-Iw/edit#gid=818998992
+
+Built on Hono (https://hono.dev/getting-started/cloudflare-workers) and Cloudflare Workers
+
 ```
 npm install
 npm run dev
--- src/index.js
+++ src/index.js
@@ -60,6 +60,84 @@ const fetchAll = async (fetchConfigs, c) => {
 
 // Root route
 app.get('/', async (c) => {
+  // Perform all the fetches
+  const responses = await fetchAll(fetchConfigs, c)
+
+  // Generate table for each sheet
+  let tables = '';
+  for (let sheet of sheets) {
+    if (responses[sheet].data.length > 0) {
+      // Get keys from the first object in the data array
+      const keys = Object.keys(responses[sheet].data[0]);
+
+      // Generate table headers
+      const headers = keys.map(key => `<th class="px-4 py-2 text-left">${key}</th>`).join('');
+
+      // Generate table rows
+      const rows = responses[sheet].data.map(item => `
+        <tr>
+          ${keys.map(key => {
+            if (typeof item[key] === 'string' && item[key].startsWith('http')) {
+              return `<td class="border px-4 py-2"><a class="text-blue-500 cursor-pointer hover:underline" href="${item[key]}">${item[key]}</a></td>`
+            } else {
+              return `<td class="border px-4 py-2">${item[key]}</td>`
+            }
+          }).join('')}
+        </tr>
+      `).join('');
+
+      // Add table to tables string
+      tables += `
+        <h2 class="text-2xl capitalize m-8 mt-16">${sheet}</h2>
+        <div class="overflow-x-auto">
+          <table class="text-sm table-auto mx-auto max-w-3xl mx-auto">
+            <thead>
+              <tr>
+                ${headers}
+              </tr>
+            </thead>
+            <tbody>
+              ${rows}
+            </tbody>
+          </table>
+        </div>
+      `;
+    }
+  }
+
+  // Return HTML content
+  return c.html(`
+    <html>
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+      </head>
+      <body class=" py-5 max-w-3xl mx-auto">
+        <h1 class="text-4xl text-center pb-3">Open Phage Data Sheet</h1>
+        <p class="text-gray-700 pb-3">This data sheet is cached + generated from the original Open Phage Google Sheet. Please contribute if you can! Every bit helps! <a class="text-blue-500 cursor-pointer hover:underline" href="https://open.phage.directory/sheet">https://open.phage.directory/sheet</a> <br><span class="text-sm">(Original Google sheets link: <a class="text-blue-500 cursor-pointer hover:underline" href="https://docs.google.com/spreadsheets/d/1fhBigiisdCc8-YWD4K8U6yvXK47p67KzA7JfAWsh-Iw/edit#gid=1571383366">https://docs.google.com/spreadsheets/d/1fhBigiisdCc8-YWD4K8U6yvXK47p67KzA7JfAWsh-Iw/edit#gid=1571383366</a> ).</span></p>
+        <p class="text-gray-700 pb-3">To access the data (for bioinformatics, scraping, LLMs, etc.) use: <a class="text-blue-500 cursor-pointer hover:underline" href="https://open.phage.directory/api">https://open.phage.directory/api</a> — the results are updated and cached every so often.</p>
+        <p class="text-gray-700 pb-3">Code can be found at: <a class="text-blue-500 cursor-pointer hover:underline" href="https://github.com/janzheng/openphage">https://github.com/janzheng/openphage</a> — the results are updated and cached every so often. This project relies on Hono and SpreadAPI to work.</p>
+        <div class="max-w-3xl mx-auto">
+          ${tables}
+        </div>
+      </body>
+      <style>
+        body {
+          font-family: sans-serif;
+        }
+      </style>
+    </html>
+  `);
+});
+
+
+app.get('/sheet', async (c) => {
+  // Redirect to the Google Sheets URL
+  return c.redirect('https://docs.google.com/spreadsheets/d/1fhBigiisdCc8-YWD4K8U6yvXK47p67KzA7JfAWsh-Iw')
+})
+
+
+// API route
+app.get('/api', async (c) => {
   try {
     // Perform all the fetches
     const responses = await fetchAll(fetchConfigs, c)
